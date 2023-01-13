e4783d01b7d8cf7dedecc7a2d4a86fca582a3859
Josh Wills <josh.wills@gmail.com>
2023-01-13 13:08:19 -0800

    Add that stuff in

--- duckdbt/api.py
+++ duckdbt/api.py
@@ -15,11 +15,11 @@ app = FastAPI(title="DuckDBT")
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
-app.mount(
-    "/static",
-    StaticFiles(directory=BASE_PATH / "static"),
-    name="static",
-)
+#app.mount(
+#    "/static",
+#    StaticFiles(directory=BASE_PATH / "static"),
+#    name="static",
+#)
 
 
 def bv_handle() -> AdapterHandle:
@@ -31,36 +31,35 @@ def bv_handle() -> AdapterHandle:
 
 
 @app.get("/")
-def index(request: Request, handle = Depends(bv_handle)):
-    qr = handle.execute_sql("SELECT table_schema, table_name FROM information_schema.tables ORDER BY 1, 2")
+def index(request: Request, handle=Depends(bv_handle)):
+    qr = handle.execute_sql(
+        "SELECT table_schema, table_name, column_name FROM information_schema.columns ORDER BY table_schema, table_name, ordinal_position"
+    )
     if qr.has_results():
-        relations = [f"{s}.{t}" for (s, t) in qr.rows()]
+        res = {}
+        for row in qr.rows():
+            relation = f"{row[0]}.{row[1]}"
+            if relation not in res:
+                res[relation] = []
+            res[relation].append(row[2])
     else:
-        relations = []
-    return TEMPLATES.TemplateResponse("index.html", {"request": request, "relations": relations})
-
-
-@app.get("/api/list_columns")
-def list_columns(relation: str, handle = Depends(bv_handle)):
-    try:
-        schema, table = relation.split(".")
-        qr = handle.execute_sql("SELECT column_name FROM information_schema.columns WHERE table_schema = ? and table_name = ? ORDER BY ordinal_position", params=(schema, table))
-        if qr.has_results():
-            return [r[0] for r in qr.rows()]
-        return []
-    except Exception as e:
-        raise HTTPException(status_code=404, detail=str(e))
+        res = {}
+    return TEMPLATES.TemplateResponse(
+        "index.html", {"request": request, "relations": res}
+    )
 
 
 class QueryRequest(BaseModel):
     sql: str
 
+
 class QueryResult(BaseModel):
     columns: List[str]
     rows: List[List[Any]]
 
+
 @app.post("/api/query", response_model=QueryResult)
-def query(q: QueryRequest, handle = Depends(bv_handle)):
+def query(q: QueryRequest, handle=Depends(bv_handle)):
     try:
         query_result = handle.execute_sql(q.sql)
         if query_result.has_results():
--- duckdbt/server.py
+++ duckdbt/server.py
@@ -1,4 +1,3 @@
-
 from typing import Any, Dict
 
 from buenavista.adapter import Adapter
@@ -21,6 +20,7 @@ def _duckdb_adapter(config: Dict[str, Any]) -> Adapter:
 
 def _postgres_adapter(config: Dict[str, Any]) -> Adapter:
     from buenavista.backends.postgres import PGAdapter
+
     kwargs = {}
     for k in ("host", "port", "user", "password", "dbname"):
         if k in config:
@@ -28,7 +28,9 @@ def _postgres_adapter(config: Dict[str, Any]) -> Adapter:
     return PGAdapter(**kwargs)
 
 
-def create(bv_config: Dict[str, Any], backing_config: Dict[str, Any]) -> BuenaVistaServer:
+def create(
+    bv_config: Dict[str, Any], backing_config: Dict[str, Any]
+) -> BuenaVistaServer:
     backing_type = backing_config["type"]
     if backing_type == "postgres":
         adapter = _postgres_adapter(backing_config)
@@ -36,7 +38,7 @@ def create(bv_config: Dict[str, Any], backing_config: Dict[str, Any]) -> BuenaVi
         adapter = _duckdb_adapter(backing_config)
     else:
         raise Exception(f"Unsupported backing type for Buena Vista: {backing_type}")
-    
+
     server_address = (bv_config.get("host"), bv_config.get("port"))
     return BuenaVistaServer(server_address, adapter, extensions=[DbtPythonRunner()])
 
@@ -106,7 +108,12 @@ if __name__ == "__main__":
         bv_server_thread.daemon = True
         bv_server_thread.start()
 
-        uvicorn.run(app, host=bv_dict.get("host"), port=bv_dict.get("api_port"), log_level=bv_dict.get("log_level", "info"))
+        uvicorn.run(
+            app,
+            host=bv_dict.get("host"),
+            port=bv_dict.get("api_port"),
+            log_level=bv_dict.get("log_level", "info"),
+        )
     else:
         print(f"Starting Buena Vista server at {bv_server.server_address}")
         bv_server.serve_forever()
